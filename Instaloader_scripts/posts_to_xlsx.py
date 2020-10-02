685134ad890da55bd7daf99af07ec80cbd89b5f4
AdriaPadilla <adrian.padilla.m@gmail.com>
2020-10-02 10:09:58 +0200

    Update posts_to_xlsx.py
    Instaloader no longer offers the username. Now we only have the account ID. This numbering guarantees a certain level of anonymization. Username can be retrieved using other installer modules.

--- Instaloader_scripts/posts_to_xlsx.py
+++ Instaloader_scripts/posts_to_xlsx.py
@@ -13,16 +13,16 @@ def to_xlsx(file):
 		except (IndexError, KeyError):
 			info["post_text"] = "null"
 		try:
-			info["account_name"] = data["node"]["owner"]["edge_felix_video_timeline"]["edges"][0]["node"]["owner"]["username"]
+			info["account_id"] = data["node"]["owner"]["id"]
 		except KeyError:
-			info["account_name"] = "null"
+			info["account_id"] = "null"
 		try:
 			info["shortcode"] = data["node"]["shortcode"]
 		except KeyError:
 			info["shortcode"] = "null"
 		try:
 			dt = data["node"]["taken_at_timestamp"]
-			info["timestamp"] = datetime.fromtimestamp(dt)
+			info["timestamp"] = datetime.fromtimestamp(dt) # convert timestamp to date time
 		except KeyError:
 			info["timestamp"] = "null"
 		try:
@@ -41,10 +41,9 @@ def to_xlsx(file):
 			info["comments_disabled"] = data["node"]["comments_disabled"]
 		except KeyError:
 			info["comments_disabled"] = "null"
-		return info
-
+		
 post_text_list = []
-account_name_list = []
+account_id_list = []
 shortcode_list = []
 timestamp_list = []
 like_count_list = []
@@ -52,14 +51,14 @@ comment_count_list = []
 video_view_count_list = []
 comments_disabled_list = []
 
-json_files = glob.glob("your_path_to_json_files/*.json")
+json_files = glob.glob("your_path_to_jsons/*.json")
 
 for json_file in json_files:
 	to_xlsx(json_file)
 	print(json_file)
 
 	post_text_list.append(info["post_text"])
-	account_name_list.append(info["account_name"])
+	account_id_list.append(info["account_id"])
 	shortcode_list.append(info["shortcode"])
 	timestamp_list.append(info["timestamp"])
 	like_count_list.append(info["like_count"])
@@ -69,7 +68,7 @@ for json_file in json_files:
 
 df = pd.DataFrame({
 	"text":post_text_list,
-	"account":account_name_list,
+	"account_id":account_id_list,
 	"shortcode":shortcode_list,
 	"timestamp":timestamp_list,
 	"like_count":like_count_list,
@@ -78,6 +77,6 @@ df = pd.DataFrame({
 	"comments_disabled":comments_disabled_list,
 	})
 
-df.to_excel("your_output_file.xlsx")
+df.to_excel("your_filename.xlsx")
 
 print(df)
